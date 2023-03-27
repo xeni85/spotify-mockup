033ba930c1f6f5ac09951255002e00cf275a060b
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-03-27 12:49:24 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { useEffect} from 'react';
+import axios from 'axios';
+import React, { useState} from 'react';
 import './App.css';
 
 
 function App(props) {
-
+  const apiKey = '6eef24d70a4c050c9ff00bf562fe4d4e'
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
   return (
     <div className="App">
-      {urlToken ? <Body /> : <Login />}
+
     </div>
   );
 }
 
 export default App;
 
-/*We have used an useEffect block so that the set of code runs only once the page is loaded.
-We have used the getTokenFromUrl function which we made in our Spotify Configuration file.
-For security purposes, we reset the URL bar to NOT show the access token so that only the app knows the access token. It’s always a good practice to hide access tokens.
-We are setting the state with the token in it. Then while rendering, we are checking if a token exists, if one exists, that means the user is logged in and should be shown a player, which we will make soon, or else the person should see the login screen.*/
