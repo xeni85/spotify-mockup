f633821e254d0e11ffc8750c80f610af1f6d67ce
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-03-27 13:18:27 -0400

    did some work on css

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import './App.css';
 
 
 function App(props) {
-  const apiKey = '6eef24d70a4c050c9ff00bf562fe4d4e'
+  const apiKey = 'f303eedcd1191e5a406fabd4f0fb671e'
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
   return (
     <div className="App">
-
+      <div className='container'>
+        <div className='upper'>
+          <div className='location'>
+            <h3>New York</h3>
+          </div>
+          <div className='weather'>
+            <h1>60°F</h1>
+          </div>
+          <div className='desc'>
+            <h1>Sunny</h1>
+          </div>
+        </div>
+        <div className='lower'>
+          <div className='feels-like'>
+            <p>60°F</p>
+          </div>
+          <div className='humidity'>
+            <p>20%</p>
+          </div>
+          <div className='wind'>
+            <p>12MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
