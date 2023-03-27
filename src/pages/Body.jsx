7bfc34c81f62e87e7880f99cc492c269f832a360
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-03-27 05:08:07 -0400

    Working hard

--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import { urlToken } from '../utils/spotifyAPI'
-import useAuth from '../components/useAuth'
+import React, { useState, useEffect } from 'react'
+import SpotifyWebApi from 'spotify-web-api-js'
 
-function Body({urlToken}) {
-    const accessToken = useAuth(urlToken)
+const spotifyApi = new SpotifyWebApi({
+    clientId: '951e602a8f6b49dca92fd775c3bd9b9a'
+})
+function Body() {
+    const [search, setSearch] = useState('')
+    const [searchResults, setSearchResults] = useState('')
+
+    useEffect(() => {
+        
+    },[search])
+    return (
+        <div>
+            <form>
+                <input type="search" placeholder='Search Songs/Artists' value={search} onChange={e => setSearch(e.target.value)}/>
+            </form>
+        </div>
+    )
 }
 
 export default Body
