080c935af91c61ebbc7771bd0358268a637a643c
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-03-27 01:59:04 -0400

    still working on login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from './components/Login';
 
 function App() {
+  useEffect(() => {
+    const hash = window.location.hash
+    if(hash){
+      const token = hash.su
+    }
+  }, [])
+  
   return (
     <div className="App">
       <Login />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,18 +10,42 @@ const styles = {
 }
 
 const imgStyles = {
-    height: '20vh'
+    height: '40vh'
 }
 
 const btnStyles = {
     border: 'none',
-    backgroundColor: 'black'
+    backgroundColor: 'black',
+    borderRadius: '20px',
+    color: 'white',
+    height: '8vh',
+    width: '18vw',
+    fontSize: '1em',
+    fontWeight: 'bolder'
+
 }
 function Login() {
+    const handleClick = () => {
+        const client_id = '951e602a8f6b49dca92fd775c3bd9b9a'
+        const redirect_url = 'http://localhost:3000/'
+        const api_url = 'https://accounts.spotify.com/authorize'
+        const scope = [
+            'user-read-private',
+            'user-read-email',
+            'user-modify-playback-state',
+            'user-read-playback-state',
+            'user-read-currently-playing',
+            'user-read-recently-played',
+            'user-top-read'
+        ]
+        window.location.href = `${api_url}?client_id=${client_id}&redirect_uri=${redirect_url}&scope=${scope.join(
+            " "
+        )}&response_type=token&show_dialog=true`
+    }
   return (
     <div style={styles}>
         <img  style={imgStyles} src='https://zeevector.com/wp-content/uploads/Spotify-Logo-PNG.png' alt=''/>
-        <button>Login</button>
+        <button style={btnStyles} onClick={handleClick}>Login</button>
     </div>
   )
 }
